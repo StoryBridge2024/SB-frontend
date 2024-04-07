5c60834f0050d6bf0f896ec5c88d9e4d1d004374
lee-haeseung <lhser123@cau.ac.kr>
2024-04-07 22:05:46 +0900

    character 제작 home
    캐릭터 제작 시작부 & 얼굴 어떻게 제작할지 묻는거

---
+++ lib/makingCharacter.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+class MakeCharacter extends StatelessWidget {
+  const MakeCharacter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          color: Color.fromARGB(0xFF, 0xB9, 0xEE, 0xFF),
+          child: Column(
+            children: [
+              Container(
+                alignment: Alignment.topLeft,
+                margin: EdgeInsets.fromLTRB(20, 20, 0, 0),
+                child: Text(
+                  '캐릭터 제작하기',
+                  style: TextStyle(
+                      fontSize: 60,
+                      color: Color.fromARGB(0xFF, 0x3B, 0x2F, 0xCA)),
+                ),
+              ),
+              Expanded(
+                  child: Scaffold(
+                      backgroundColor: Color(0x00000000),
+                      body: Container(
+                          width: double.infinity,
+                          height: double.infinity,
+                          color: Color(0xffffffff),
+                          margin: EdgeInsets.all(25),
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Flexible(
+                                    flex: 1,
+                                    child: Container(
+                                      width: double.infinity,
+                                      height: double.infinity,
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        '먼저 얼굴을 제작해야 해요!',
+                                        style: TextStyle(fontSize: 70),
+                                      ),
+                                    )),
+                                Flexible(
+                                    flex: 2,
+                                    child: Container(
+                                      width: double.infinity,
+                                      height: double.infinity,
+                                      child: Row(
+                                        children: [
+                                          Flexible(
+                                              flex: 1,
+                                              child: Container(
+                                                  width: double.infinity,
+                                                  height: double.infinity,
+                                                  alignment: Alignment.center,
+                                                  child: ElevatedButton(
+                                                    style: ElevatedButton.styleFrom(
+                                                        backgroundColor:
+                                                        Color(0xFFFFFFFF),
+                                                        surfaceTintColor:
+                                                        Color(0xFFFFFFFF),
+                                                        shape:
+                                                        BeveledRectangleBorder(),
+                                                        elevation: 0,
+                                                        foregroundColor:
+                                                        Color(0x00000000),
+                                                        padding:
+                                                        EdgeInsets.all(30)),
+                                                    child: Image.asset(
+                                                      'assets/image/img_2.png',
+                                                      width: 250,
+                                                    ),
+                                                    onPressed: () {
+
+                                                    },
+                                                  ))),
+                                          Flexible(
+                                              flex: 1,
+                                              child: Container(
+                                                  width: double.infinity,
+                                                  height: double.infinity,
+                                                  alignment: Alignment.center,
+                                                  child: ElevatedButton(
+                                                    style: ElevatedButton.styleFrom(
+                                                        backgroundColor:
+                                                        Color(0xFFFFFFFF),
+                                                        surfaceTintColor:
+                                                        Color(0xFFFFFFFF),
+                                                        shape:
+                                                        BeveledRectangleBorder(),
+                                                        elevation: 0,
+                                                        foregroundColor:
+                                                        Color(0x00000000),
+                                                        padding:
+                                                        EdgeInsets.all(30)),
+                                                    child: Image.asset(
+                                                      'assets/image/img_3.png',
+                                                      width: 300,
+                                                    ),
+                                                    onPressed: () {
+
+                                                    },
+                                                  )))
+                                        ],
+                                      ),
+                                    ))
+                              ]))))
+            ],
+          )),
+    );
+  }
+}
