f7082abed9fcd435d55ef2ce6b73f01511663da9
kimju0 <kimuniverse0@naver.com>
2024-04-13 19:03:07 +0900

    ftr: text generation api main.dart에서 api호출하고 콘솔에 출력

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:frontend/services/api/openai.dart';
 import './homePage.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
+  OpenAI openAI = OpenAI();
+  var response = await openAI.createCompletion();
+  print(response.body);
   runApp(
     const MyApp(),
   );
