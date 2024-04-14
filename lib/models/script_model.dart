af56a81d6f7cca159ca5b26144c4358e581e33ca
kimju0 <kimuniverse0@naver.com>
2024-04-15 02:23:54 +0900

    ftr: make model of api response 응답 형식에 맞게 model 만들고 decode해서 class에 저장

---
+++ lib/models/script_model.dart
@@ -0,0 +1,14 @@
+class ScriptModel{
+  final String id,model,system_fingerprint,object;
+  final int created;
+  final List<dynamic> choices;
+  final usage;
+  ScriptModel.fromJson(Map<String,dynamic> json):
+    id = json['id'],
+    created = json['created'],
+    model = json['model'],
+    system_fingerprint = json['system_fingerprint'],
+    object = json['object'],
+    choices = json['choices'],
+    usage = json['usage'];
+}
--- lib/services/api/openai_api.dart
+++ lib/services/api/openai_api.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:frontend/models/script_model.dart';
 import 'package:http/http.dart';
 import '../../constants/prompt.dart';
 
@@ -7,7 +8,7 @@ class OpenAI {
   final String? apiKey = dotenv.env['OPENAI_APIKEY'];
   final String baseUrl = 'https://api.openai.com/v1/';
 
-  Future<Response> createCompletion() async {
+  Future<ScriptModel> createCompletion() async {
     final response = await post(
       Uri.parse('${baseUrl}chat/completions'),
       headers: {
@@ -16,6 +17,10 @@ class OpenAI {
       },
       body: json.encode(textPrompt),
     );
-    return response;
+    if(response.statusCode != 200){
+      throw Exception('Failed to load response');
+    }
+    ScriptModel scriptModel = ScriptModel.fromJson(json.decode(response.body));
+    return scriptModel;
   }
 }
